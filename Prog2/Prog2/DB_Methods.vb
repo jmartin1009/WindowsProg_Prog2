cf34df40dc509023df0fc8973c51a28c19e6bb84
Jaelen <Martinjae@uwplatt.edu>
2021-03-22 18:34:41 -0500

    Created Backend Class
    Set up backend class and called its methods from main form. Methods still have to be set up with SQL queries and stuff

---
+++ Prog2/Prog2/DB_Methods.vb
@@ -0,0 +1,14 @@
+﻿Public Class DB_Methods
+    Public Sub fnShow(sender As Form1)
+        sender.dvgEmployee.Refresh() 'NOT CORRECT, SHOULD PROLLY DO A SELECT * TO REFRESH
+    End Sub
+
+    Public Sub fnDelete(userID As String)
+
+    End Sub
+
+    Public Sub fnInsert(userID As String, userName As String, userSalary As String, userAddress As String)
+
+    End Sub
+
+End Class
--- Prog2/Prog2/Form1.vb
+++ Prog2/Prog2/Form1.vb
@@ -1,13 +1,14 @@
 ﻿Public Class Form1
+    Private db As New DB_Methods
     Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
-
+        db.fnShow(Me)
     End Sub
 
     Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
-
+        db.fnDelete(txtDeleteID.Text)
     End Sub
 
     Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
-
+        db.fnInsert(txtID.Text, txtName.Text, txtSalary.Text, txtAddress.Text)
     End Sub
 End Class
