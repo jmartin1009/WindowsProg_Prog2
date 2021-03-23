9b209d0a6400e8f621de1aa0a6588a560afd68fb
Jaelen <Martinjae@uwplatt.edu>
2021-03-23 16:05:45 -0500

    Backend Changes

--- Prog2/Prog2/DB_Methods.vb
+++ Prog2/Prog2/DB_Methods.vb
@@ -1,6 +1,12 @@
-﻿Public Class DB_Methods
+﻿Imports System
+Imports System.Data
+Imports System.Data.OleDb
+Public Class DB_Methods
+    Private conFlag As Boolean = False
+    Private con As OleDbConnection = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\employeeDB.mdb")
     Public Sub fnShow(sender As Form1)
-        sender.dvgEmployee.Refresh() 'NOT CORRECT, SHOULD PROLLY DO A SELECT * TO REFRESH
+        'sender.dvgEmployee.Refresh() 'NOT CORRECT, SHOULD PROLLY DO A SELECT * TO REFRESH
+        connectionCheck()
     End Sub
 
     Public Sub fnDelete(userID As String)
@@ -9,6 +15,22 @@
 
     Public Sub fnInsert(userID As String, userName As String, userSalary As String, userAddress As String)
 
+    End Sub
+
+    Public Sub connectionCheck()
+        If (conFlag = False) Then
+            con.Open()
+            conFlag = True
+        End If
+        Try
+            If con.State = ConnectionState.Open Then
+                MsgBox("Connected")
+            Else
+                Throw New Exception("Database Connection failed")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
     End Sub
 
 End Class
