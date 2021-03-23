92b79babf4198dba498c6b5b050c724f92f0abb7
Jaelen <Martinjae@uwplatt.edu>
2021-03-23 18:13:18 -0500

    Finished Program
    :)

--- Prog2/Prog2/DB_Methods.vb
+++ Prog2/Prog2/DB_Methods.vb
@@ -4,33 +4,71 @@ Imports System.Data.OleDb
 Public Class DB_Methods
     Private conFlag As Boolean = False
     Private con As OleDbConnection = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\employeeDB.mdb")
+    'Handles showing the database table contents
+    'Contains code from Argo's example
     Public Sub fnShow(sender As Form1)
-        'sender.dvgEmployee.Refresh() 'NOT CORRECT, SHOULD PROLLY DO A SELECT * TO REFRESH
         connectionCheck()
+        Dim cmd = doCommand("Select * from tblEmployee")
+
+        'Fire the command using data adapter
+        Dim da As New OleDb.OleDbDataAdapter
+        da.SelectCommand = cmd
+
+        'Fill it in a temporary table
+        Dim dt As New DataTable
+        da.Fill(dt)
+
+        'Display in the front end
+        sender.dvgEmployee.DataSource = dt
+
     End Sub
-
-    Public Sub fnDelete(userID As String)
-
+    'Handles deleting a row from the table based on ID entered
+    Public Sub fnDelete(userID As String, sender As Form1)
+        connectionCheck()
+        Dim cmd = doCommand("DELETE FROM tblEmployee WHERE empID='" & userID & "'")
+        Try
+            Dim check = cmd.ExecuteNonQuery
+            If check < 0 Then
+                Throw New Exception("Query Failed")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        fnShow(sender)
     End Sub
-
-    Public Sub fnInsert(userID As String, userName As String, userSalary As String, userAddress As String)
-
+    'Handles adding a row to the table given the inputs from the user
+    Public Sub fnInsert(userID As String, userName As String, userSalary As Integer, userAddress As String, sender As Form1)
+        connectionCheck()
+        Dim cmd = doCommand("INSERT INTO tblEmployee (empID, empName, empSalary, empAddress) VALUES ('" & userID & "', '" & userName & "', '" & userSalary & "', '" & userAddress & "')")
+        Try
+            Dim check = cmd.ExecuteNonQuery
+            If check < 0 Then
+                Throw New Exception("Query Failed")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        fnShow(sender)
     End Sub
-
+    'Creates and returns a command given the query string
+    Private Function doCommand(query As String)
+        Dim cmd As New OleDb.OleDbCommand
+        cmd.Connection = con
+        cmd.CommandText = query
+        Return cmd
+    End Function
+    'Checks connection to database, only opens connection if connection flag indicates a connection has not been made
     Public Sub connectionCheck()
         If (conFlag = False) Then
             con.Open()
             conFlag = True
         End If
         Try
-            If con.State = ConnectionState.Open Then
-                MsgBox("Connected")
-            Else
+            If Not (con.State = ConnectionState.Open) Then
                 Throw New Exception("Database Connection failed")
             End If
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
     End Sub
-
 End Class
--- Prog2/Prog2/Form1.vb
+++ Prog2/Prog2/Form1.vb
@@ -1,14 +1,15 @@
 ﻿Public Class Form1
     Private db As New DB_Methods
+    'Handles user clicking the show button
     Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
         db.fnShow(Me)
     End Sub
-
+    'Handles user clicking the delete button
     Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
-        db.fnDelete(txtDeleteID.Text)
+        db.fnDelete(txtDeleteID.Text, Me)
     End Sub
-
+    'Handles user clicking the insert button
     Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
-        db.fnInsert(txtID.Text, txtName.Text, txtSalary.Text, txtAddress.Text)
+        db.fnInsert(txtID.Text, txtName.Text, Convert.ToInt32(txtSalary.Text), txtAddress.Text, Me)
     End Sub
 End Class
